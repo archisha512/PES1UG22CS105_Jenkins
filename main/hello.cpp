11240172fb59f3d17599f7c81095be50dc4b0891
archisha512 <141921818+archisha512@users.noreply.github.com>
2025-03-18 16:37:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Archisha from Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
